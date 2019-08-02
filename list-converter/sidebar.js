c6a077dfc2f6e8b11276d87b63b6529ed40b5b9c
BobMoonbird <boris.borodyanskii@gmail.com>
2019-08-02 19:01:16 +0500

    Update sidebar.js
    do not create first and last empty stickiy

--- list-converter/sidebar.js
+++ list-converter/sidebar.js
@@ -4,7 +4,7 @@ rtb.onReady(() => {
 })
 
 async function getWidget() {
-	let widget = await rtb.board.getSelection()
+	let widget = await rtb.board.election.get()
 	let text = widget[0]['text']
 	const widgetText = document.getElementById('widget-text')
 	widgetText.value = String(text)
@@ -18,7 +18,7 @@ async function convertFromList() {
 	var newText = document.getElementById("widget-text").value
 	stickiesList = newText.split("</li>")
 	
-	for (var i = 0; i < stickiesList.length; i++) {
+	for (var i = 1; i < stickiesList.length-1; i++) {
 		rtb.board.widgets.stickers.create({text: stickiesList[i], x: xCoord, y: yCoord+100})
 		xCoord = xCoord+200
 
