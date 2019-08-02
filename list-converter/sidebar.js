f67d440f3b6c962fa88357c089a9f423faf50db2
BobMoonbird <boris.borodyanskii@gmail.com>
2019-08-02 19:28:24 +0500

    Update sidebar.js

--- list-converter/sidebar.js
+++ list-converter/sidebar.js
@@ -4,21 +4,22 @@ rtb.onReady(() => {
 })
 
 async function getWidget() {
-	let widget = await rtb.board.election.get()
+	let widget = await rtb.board.selection.get()
 	let text = widget[0]['text']
 	const widgetText = document.getElementById('widget-text')
 	widgetText.value = String(text)
 }
 
 async function convertFromList() {
-	let widget = await rtb.board.getSelection()
+	let widget = await rtb.board.selection.get()
 	xCoord = widget[0].x
 	yCoord = widget[0].y
 	
 	var newText = document.getElementById("widget-text").value
 	stickiesList = newText.split("</li>")
+	element = sticliesList.pop()
 	
-	for (var i = 1; i < stickiesList.length-1; i++) {
+	for (var i = 0; i < stickiesList.length; i++) {
 		rtb.board.widgets.stickers.create({text: stickiesList[i], x: xCoord, y: yCoord+100})
 		xCoord = xCoord+200
 
