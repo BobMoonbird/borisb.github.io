38407eda77798b738b43012b23f711d37ca33f00
Boris Borodyansky <boris.borodyanskii@gmail.com>
2020-02-11 15:26:34 +0500

    Update main.js

--- test/main.js
+++ test/main.js
@@ -2,7 +2,7 @@ rtb.onReady(() => {
 	 rtb.initialize({
 		extensionPoints: {
 			toolbar:{
-				title:'List Converter',
+				title:'Test Converter',
 				toolbarSvgIcon:'<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
 				librarySvgIcon:'<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
 				onClick: async function() {
