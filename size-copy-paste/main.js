650883682daf84281d33385dd7c3b05526afac8f
Boris Borodyansky <boris.borodyanskii@gmail.com>
2020-02-13 16:33:59 +0500

    Update main.js

--- size-copy-paste/main.js
+++ size-copy-paste/main.js
@@ -6,7 +6,7 @@ miro.onReady(() => {
                 if (authorized) {
                     return {
                         tooltip: 'Paste',
-                        svgIcon: '<svg><text x="0" y="15" fill="red">I love SVG!</text></svg>',
+                        svgIcon: '<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
                         onClick: onClick
                     }
                 }
