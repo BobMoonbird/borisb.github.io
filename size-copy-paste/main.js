0396ac99ecbce024e29c17a520490f2645aa3f5e
Boris Borodyansky <boris.borodyanskii@gmail.com>
2020-02-13 16:12:59 +0500

    Update main.js

--- size-copy-paste/main.js
+++ size-copy-paste/main.js
@@ -1,13 +1,12 @@
 miro.onReady(() => {
     miro.initialize({
         extensionPoints: {
-            toolbar: async () => {
+            getWidgetMenuItems: async () => {
                 const authorized = await miro.isAuthorized()
                 if (authorized) {
                     return {
-                        title: 'Size Copy Paste',
-                        toolbarSvgIcon: '<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
-                        librarySvgIcon: '<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
+                        tooltip: 'Size Copy Paste',
+                        svgIcon: '<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
                         onClick: onClick
                     }
                 }
