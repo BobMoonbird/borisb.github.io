034f00007495e1af41cc8435e5b032be4b2ecae9
Boris Borodyansky <boris.borodyanskii@gmail.com>
2020-02-14 03:09:23 +0500

    Update bottompanel.js

--- size-copy-paste/bottompanel.js
+++ size-copy-paste/bottompanel.js
@@ -1,37 +1,38 @@
 miro.onReady(async () => {
   // subscribe on user selected widgets
   //miro.addListener(miro.enums.event.SELECTION_UPDATED, getWidget)
-  console.log(234);
+
   getWidget()
   
+  await miro.addListener('WIDGETS_TRANSFORMATION_UPDATED', (e) => {  { getWidget(); console.log(321) } })
   await miro.addListener('SELECTION_UPDATED', (e) => {  { getWidget(); console.log(123) } })
 })
 
 async function getWidget() {
   // Get selected widgets
   let widgets = await miro.board.selection.get()
-      console.log(widgets);
+
   if (!widgets || !widgets.length){
     return;
   }
   
   var widgetWidth = document.getElementById('widget-width')
   var widgetHeight = document.getElementById('widget-height')
-  console.log(widgetWidth, widgetHeight)
+
   
   widgetWidth.value = widgets[0].bounds.width
   widgetHeight.value = widgets[0].bounds.height
-  console.log(widgetWidth.value, widgetHeight.value)
+
 }
 
 async function updateWidget() {
   
   let widget = await miro.board.selection.get()
-  console.log(widget[0].id)
+
   
   var newWidth = parseInt(document.getElementById("widget-width").value)
   var newHeight = parseInt(document.getElementById("widget-height").value)
-  console.log(newWidth, newHeight)
+
   
   miro.board.widgets.update({id:widget[0].id, width:newWidth, height:newHeight})
 
