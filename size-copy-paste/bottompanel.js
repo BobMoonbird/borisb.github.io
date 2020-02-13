91029eaf25c839862719d78e360a3d7a65130cd4
Boris Borodyansky <boris.borodyanskii@gmail.com>
2020-02-13 23:21:34 +0500

    Update bottompanel.js

--- size-copy-paste/bottompanel.js
+++ size-copy-paste/bottompanel.js
@@ -4,15 +4,24 @@ miro.onReady(() => {
   getWidget()
 })
 
-// Get html elements for tip and text container
-const tipElement = document.getElementById('tip')
-const widgetTextElement = document.getElementById('widget-text')
-
 async function getWidget() {
   // Get selected widgets
   let widgets = await miro.board.selection.get()
+  
+  var widgetWidth = document.getElementById('widget-width')
+  var widgetHeight = document.getElementById('widget-height')
+
+  widgetWidth.value = widgets[0].bounds.width
+  widgetHeight.value = widgets[0].bounds.height
 
   // Get first widget from selected widgets
   let text = widgets[0].text
+}
+
+async function updateWidget(width, height) {
+  
+  let widgets = await miro.board.selection.get()
+  miro.board.widgets.update(id,{bounds{width:width, height: height})
+  
 }
 
