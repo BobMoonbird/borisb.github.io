5e3814c709a94e3db741a8520eb45a2d9eb31bfe
Boris Borodyansky <boris.borodyanskii@gmail.com>
2020-02-20 06:45:06 +1100

    Update main.js

--- cleanbar/main.js
+++ cleanbar/main.js
@@ -9,7 +9,8 @@ miro.onReady(() => {
                         svgIcon: '<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
                         onClick: async () =>{
                         await miro.board.ui.__hideButtonsPanels('all')
-                        await miro.addListener(miro.enums.event.ESC_PRESSED, miro.board.ui.__showButtonsPanels('all')) }
+                        await miro.addListener(miro.enums.event.ESC_PRESSED, showAll)
+                        showAll()          }  
                     }
                 }
             }
@@ -17,3 +18,6 @@ miro.onReady(() => {
     })
 
 })
+
+async function showAll() {
+    miro.board.ui.__showButtonsPanels('all') }
