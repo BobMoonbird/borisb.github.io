5f53cd4440101ddb556a0e5d5edbc81d5c53436e
BobMoonbird <boris.borodyanskii@gmail.com>
2020-06-18 21:33:25 +0500

    filter for rendering widget menu icon

--- list-converter/main.js
+++ list-converter/main.js
@@ -7,7 +7,7 @@ miro.onReady(() => {
                 const authorized = await miro.isAuthorized()
                 if (authorized) {
                     return {
-                        title: 'List onverter',
+                        title: 'List Converter',
                         toolbarSvgIcon: '<path fill="currentColor" fill-rule="nonzero" d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
                         librarySvgIcon: '<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
                         onClick: onClick
@@ -18,11 +18,25 @@ miro.onReady(() => {
             getWidgetMenuItems: async() => {
                 const authorized = await miro.isAuthorized()
                 if (authorized) {
-                return {
-                	tooltip: 'Hi',
-			svgIcon: icon24,
-			onClick: onClick
-                }}
+                    
+                    let objects = await miro.board.selection.get()
+                    const isStickie = (widget) => widget.type === "STICKER"
+                    if (objects.every(isStickie)) {
+                        return {
+                            tooltip: 'List Converter',
+                            svgIcon: icon24,
+                            onClick: onClick
+                        }
+                    } else if (objects.length === 1 && objects[0].type === "TEXT") {
+                        return {
+                            tooltip: 'List Converter',
+                            svgIcon: icon24,
+                            onClick: onClick
+                        }
+                    }
+
+                    
+                }
 
 			}
         }
