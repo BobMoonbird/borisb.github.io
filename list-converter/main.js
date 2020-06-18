7841a5a7e62e203c1d50a8b37d4378ad637e2a83
BobMoonbird <boris.borodyanskii@gmail.com>
2020-06-18 21:11:46 +0500

    check commit

--- list-converter/main.js
+++ list-converter/main.js
@@ -7,7 +7,7 @@ miro.onReady(() => {
                 const authorized = await miro.isAuthorized()
                 if (authorized) {
                     return {
-                        title: 'List Converter',
+                        title: 'List onverter',
                         toolbarSvgIcon: '<path fill="currentColor" fill-rule="nonzero" d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
                         librarySvgIcon: '<path d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
                         onClick: onClick
