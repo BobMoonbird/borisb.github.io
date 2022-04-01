ad60613b7795dc26ea6a3b84b4736ecb9e8b0f42
Boris Borodyansky <boris.borodyanskii@gmail.com>
2022-04-02 00:23:42 +0200

    patch for v2

--- list-converter/main.js
+++ list-converter/main.js
@@ -14,7 +14,7 @@ async function init() {
               str_ara.map(v => max = Math.max(max, v.length));	
               result = str_ara.filter(v => v.length == max);	
               return result;	
-          }
+          }//
   
           let width = longest_string(texts)[0].length * 5 */
   
